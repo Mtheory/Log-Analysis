53ffe1cf07fbd32b98d962a418612e76442d8e10
Martin Chojnowski <incedofortis@gmail.com>
2017-09-18 22:06:45 +0100

    remove views one query for each question

--- README.txt
+++ README.txt
@@ -1,7 +1,9 @@
 ﻿Log report is an internal reporting tool that generate following reports from newspaper database. 
+
 - Three most popular articles of all time.
 - The most popular article authors of all time.
 - Day(s) where connection request errors exceed 1% of all connections.
+
 Information is displayed directly to the terminal.
 
 
@@ -11,36 +13,11 @@ Download the zip file and extract to a directory of your choosing.
 Files names :
 -	log_report.py
 
-The program was designed to run under Linux with PSQL database installed 
 
 Running
-Program requires database news, and Python 2.7 installed.
+The program has been written in Python 2.7 using psycopag2 module. 
+PSQL database “news” has to be present for program to run.
 
-For correct operation five views are required to be created within news database
-1.
-CREATE VIEW articles_popularity AS 
-SELECT articles.title, report.views 
-FROM articles LEFT JOIN 
-(SELECT path, COUNT(*) AS views FROM log GROUP BY path) AS report 
-ON report.path LIKE CONCAT('%',articles.slug) 
-ORDER BY report.views DESC;	
-2.
-CREATE VIEW articles_author AS 
-SELECT articles.title,authors.name 
-FROM articles JOIN authors ON articles.author = authors.id;
-3.
-CREATE VIEW all_connection AS 
-(SELECT CAST(time AS date) AS day, COUNT(*) AS num 
-FROM log GROUP BY day);
-4.
-CREATE VIEW error_connection AS 
-(SELECT CAST(time AS date) AS day , COUNT(*) AS num 
-FROM log WHERE status NOT LIKE '200 OK' GROUP BY day);
-5.
-CREATE VIEW errors_per_day As 
-SELECT all_connection.day,
-CAST(CAST(error_connection.num AS FLOAT)/CAST(all_connection.num AS FLOAT)*100 AS NUMERIC(18,2)) AS p 
-FROM all_connection LEFT JOIN error_connection 
-ON all_connection.day = error_connection.day;
+
 
 
--- log_report.py
+++ log_report.py
@@ -10,9 +10,16 @@ print "\nReports:"
 # display three most popular articles
 def articles():
     db = psycopg2.connect(database=DBNAME)
-    cursor = db.cursor()
-    cursor.execute("select * from articles_popularity limit 3")
-    rows = cursor.fetchall()
+    c = db.cursor()
+    c.execute("WITH articles_popularity AS " +
+              "(SELECT articles.title, report.views FROM " +
+              "articles LEFT JOIN " +
+              "(SELECT path,COUNT(*) AS views FROM log GROUP BY path) " +
+              "AS report ON report.path LIKE CONCAT('%',articles.slug) " +
+              "ORDER BY report.views DESC) " +
+              "SELECT * FROM articles_popularity LIMIT 3")
+    rows = c.fetchall()
+    # print results to the terminal with visual formating
     print 'Three most populart articles :'
     for row in rows:
         offset = 35 - len(row[0])
@@ -26,12 +33,22 @@ def articles():
 # display most popular authors
 def authors():
     db = psycopg2.connect(database=DBNAME)
-    cursor = db.cursor()
-    cursor.execute("select name,sum(views)" +
-                   " from articles_popularity,articles_author " +
-                   "where articles_popularity.title = articles_author.title" +
-                   " group by name order by sum(views) desc;")
-    rows = cursor.fetchall()
+    c = db.cursor()
+    c.execute("WITH articles_popularity AS " +
+              "(SELECT articles.title, report.views FROM " +
+              "articles LEFT JOIN " +
+              "(SELECT path,COUNT(*) AS views FROM log GROUP BY path) " +
+              "AS report ON report.path LIKE CONCAT('%',articles.slug) " +
+              "ORDER BY report.views DESC), " +
+              "articles_author AS " +
+              "(SELECT articles.title,authors.name FROM " +
+              "articles JOIN authors ON articles.author = authors.id) " +
+              "SELECT name,SUM(views) FROM " +
+              "articles_popularity,articles_author WHERE " +
+              "articles_popularity.title = articles_author.title " +
+              "GROUP BY name ORDER BY SUM(views) DESC")
+    rows = c.fetchall()
+    # print results to the terminal with visual formating
     print 'Most popular article authors :'
     for row in rows:
         offset = 35 - len(row[0])
@@ -45,14 +62,29 @@ def authors():
 # display days with more than 1% error connection
 def connection_errors():
     db = psycopg2.connect(database=DBNAME)
-    cursor = db.cursor()
-    cursor.execute("select * from errors_per_day where p>1;")
-    rows = cursor.fetchall()
+    c = db.cursor()
+    c.execute("WITH all_connection AS " +
+              "(SELECT CAST(time AS date) AS day, COUNT(*) AS num " +
+              "FROM log GROUP BY day), " +
+              "error_connection AS " +
+              "(SELECT CAST(time AS date) AS day , COUNT(*) AS num " +
+              "FROM log WHERE status NOT LIKE '200 OK' GROUP BY day), " +
+              "errors_per_day AS"
+              "(SELECT all_connection.day, " +
+              "CAST(CAST(error_connection.num AS FLOAT)/" +
+              "CAST(all_connection.num AS FLOAT)*100 AS NUMERIC(18,2)) " +
+              "AS percentage FROM " +
+              "all_connection LEFT JOIN error_connection " +
+              "ON all_connection.day = error_connection.day) " +
+              "SELECT * FROM errors_per_day WHERE percentage > 1")
+    rows = c.fetchall()
+    # print results to the terminal
     print "Days with more than 1% connection errors :"
     for row in rows:
         print "\t* ", row[0], "\t\t-", row[1], ' %'
     db.close()
     print '\n'
+
 
 # functions execution
 articles()
